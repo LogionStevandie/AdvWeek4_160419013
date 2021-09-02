08365b74f4afbb06ad8bfbfa111a5bd75a26c976
LogionStevandie <s160419013@student.ubaya.ac.id>
2021-09-02 20:57:32 +0700

    Exercise

--- app/src/main/java/id/ac/ubaya/informatika/advweek4/view/StudentDetailFragment.kt
+++ app/src/main/java/id/ac/ubaya/informatika/advweek4/view/StudentDetailFragment.kt
@@ -5,9 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
 import id.ac.ubaya.informatika.advweek4.R
+import id.ac.ubaya.informatika.advweek4.model.Student
+import id.ac.ubaya.informatika.advweek4.viewmodel.DetailViewModel
+import id.ac.ubaya.informatika.advweek4.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.fragment_student_detail.*
 
 class StudentDetailFragment : Fragment() {
+    private lateinit var viewModel: DetailViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,5 +27,20 @@ class StudentDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+        viewModel.fetch()
+
+        viewModel.studentLD.observe(viewLifecycleOwner, Observer {
+            txtId.setText(it.id)
+            txtName.setText(it.name)
+            txtBod.setText(it.bod)
+            txtPhone.setText(it.phone)
+        })
+
+        btnUpdate.setOnClickListener {
+            val action = StudentDetailFragmentDirections.actionStudentList()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 }
