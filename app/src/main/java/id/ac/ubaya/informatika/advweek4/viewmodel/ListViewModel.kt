f369169ea5c0f1efc916a5228143b935c5677730
LogionStevandie <s160419013@student.ubaya.ac.id>
2021-09-09 13:22:28 +0700

    Week 5

--- app/src/main/java/id/ac/ubaya/informatika/advweek4/viewmodel/ListViewModel.kt
+++ app/src/main/java/id/ac/ubaya/informatika/advweek4/viewmodel/ListViewModel.kt
@@ -1,16 +1,28 @@
 package id.ac.ubaya.informatika.advweek4.viewmodel
 
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import id.ac.ubaya.informatika.advweek4.model.Student
 
-class ListViewModel:ViewModel() {
+class ListViewModel(application: Application):AndroidViewModel(application) {
     val studensLD = MutableLiveData<List<Student>>()
     val loadingErrorLD = MutableLiveData<Boolean>()
-    val loadingDoneLD = MutableLiveData<Boolean>()
+    val loadingLD = MutableLiveData<Boolean>()
+
+    private val TAG = "volleyTag"
+    private var queue:RequestQueue ?= null
 
     fun refresh(){
-        val student1 = Student("16055","Nonie","1998/03/28","5718444778","http://dummyimage.com/75x100.jpg/cc0000/ffffff")
+        /*val student1 = Student("16055","Nonie","1998/03/28","5718444778","http://dummyimage.com/75x100.jpg/cc0000/ffffff")
 
         val student2 = Student("13312","Rich","1994/12/14","3925444073","http://dummyimage.com/75x100.jpg/5fa2dd/ffffff")
 
@@ -19,6 +31,34 @@ class ListViewModel:ViewModel() {
         studensLD.value = arrayListOf<Student>(student1, student2, student3)
         loadingErrorLD.value = false
         loadingDoneLD.value = true
+        */
+        loadingErrorLD.value = false
+        loadingLD.value = true
 
+        queue = Volley.newRequestQueue(getApplication())
+        var url = "http://adv.jitusolution.com/student.php"
+
+        val stringRequest = StringRequest(Request.Method.GET, url,
+            {response ->
+                val sType = object : TypeToken<List<Student>>() { }.type
+                val result = Gson().fromJson<List<Student>>(response, sType)
+                studensLD.value = result
+
+                loadingLD.value = false
+                Log.d("Show volley: ", response.toString())
+            },
+            {response ->
+                loadingErrorLD.value = true
+                loadingLD.value = false
+                Log.d("Show volley: ", response.toString())
+            })
+
+        stringRequest.tag = TAG
+        queue?.add(stringRequest)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        queue?.cancelAll(TAG)
     }
 }
