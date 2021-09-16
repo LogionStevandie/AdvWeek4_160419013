171f7228b5eb29aaa50f1eca117fe16e1e3da9b4
LogionStevandie <s160419013@student.ubaya.ac.id>
2021-09-16 13:08:30 +0700

    Week 6 Video Done

--- app/src/main/java/id/ac/ubaya/informatika/advweek4/view/StudentListAdapter.kt
+++ app/src/main/java/id/ac/ubaya/informatika/advweek4/view/StudentListAdapter.kt
@@ -7,6 +7,7 @@ import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import id.ac.ubaya.informatika.advweek4.R
 import id.ac.ubaya.informatika.advweek4.model.Student
+import id.ac.ubaya.informatika.advweek4.util.loadImage
 import kotlinx.android.synthetic.main.student_list_item.view.*
 
 class StudentListAdapter(val studentList:ArrayList<Student>):RecyclerView.Adapter<StudentListAdapter.StudentViewHolder>() {
@@ -28,6 +29,8 @@ class StudentListAdapter(val studentList:ArrayList<Student>):RecyclerView.Adapte
     override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
         holder.view.txtId.text = studentList[position].id
         holder.view.txtNama.text = studentList[position].name
+        holder.view.imageView.loadImage(studentList[position].photoUrl.toString(), holder.view.progressBar)
+
 
         holder.view.btnDetail.setOnClickListener {
             val action = StudentListFragmentDirections.actionStudentDetail()
